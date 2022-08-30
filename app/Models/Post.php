74fc359eb4824553fe88084fd0637574a36c379b
natannms <agnusnat.dnms@gmail.com>
2022-08-30 00:58:08 -0300

    Adicionando relação N -> 1

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,4 +10,9 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable = ['user_id','title', 'body'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
